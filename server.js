4f0edbe7019e1a9749f7b66f0d5ec406fadf8689
joeczar <joe.czar@gmail.com>
2018-11-06 15:22:11 +0100

    added Debugging to server.js

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer(function (request, response) {
+    console.log('createServer callback');
     response.writeHead(200, { 'content-type': 'text/html; charset=utf8' });
 
     const parsedUrl = url.parse(request.url, true);
+
+    console.log(parsedUrl);
+    console.log('Name: ', parsedUrl.query.name);
 
     const body = `<!DOCTYPE html>
       	<html>
@@ -21,6 +25,7 @@ const server = http.createServer(function (request, response) {
     response.end(body);
 });
 
+console.log('listen');
 server.listen(8080, function () {
     console.log('Server is listening to http//localhost:8080');
 });
